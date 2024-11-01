40c8da7f8d7a12606b473b82b9383b7ae58222bd
Braden Little <blittle485@gmail.com>
2024-10-31 22:49:52 -0400

    Deployment using GA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: 'build',
+  output: "export",
+  basePath: "/me",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
